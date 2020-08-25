d44c285856e97e9ef1edf4a812c413085397b0e3
Abhee Hudani <hudaniabhee@gmail.com>
2020-08-25 19:15:26 +0530

    Model added with add,update,delete route

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 dotenv.config({ path: "./config/config.env" });
 const PORT = process.env.PORT || 5000;
 
-const app = express();
+connectDB();
+const transactions = require("./routes/transactions");
 
-app.get("/", (req, res) => res.send("Server is live..."));
+const app = express();
+app.use(express.json());
+app.use("/api/v1/transactions", transactions);
 
 app.listen(PORT, console.log(`Server is live... @ Port:${PORT}`.red));
